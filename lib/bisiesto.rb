c17ac631e503e22e9e30aa990ca76aab389b6432
Pablo Alvarez <79346499+p48102611@users.noreply.github.com>
2021-04-26 18:17:21 -0400

    Implementando condicional de que num no es divisible entre 100

--- lib/bisiesto.rb
+++ lib/bisiesto.rb
@@ -1,8 +1,10 @@
 def esBisiesto(num)
-    if num % 4 == 0 
-        return true
-    end
     if num % 400 == 0
         return true
+    end
+    if(num % 4 == 0 && num % 100 != 0)
+        return true
+    else
+        return false
     end
 end
--- spec/bisiesto_spec.rb
+++ spec/bisiesto_spec.rb
@@ -1,18 +1,30 @@
 require './lib/bisiesto.rb'
 RSpec.describe "bisiesto" do
-  it "deberia devolver true si 4 es divisible entre 4" do
+  it "deberia devolver true si 4 es bisiesto" do
     expect(esBisiesto(4)).to eq true
   end
-  it "deberia devolver true si 400 es divisible entre 400" do
+  it "deberia devolver true si 400 es bisiesto" do
     expect(esBisiesto(400)).to eq true
   end
-  it "deberia devolver true si 800 es divisible entre 400" do
+  it "deberia devolver true si 800 es bisiesto" do
     expect(esBisiesto(800)).to eq true
   end
-  it "deberia devolver true si 2000 es divisible entre 400" do
+  it "deberia devolver true si 2000 es bisiesto" do
     expect(esBisiesto(2000)).to eq true
   end
-  it "deberia devolver true si 2400 es divisible entre 400" do
+  it "deberia devolver true si 2400 es bisiesto" do
     expect(esBisiesto(2400)).to eq true
+  end
+  it "deberia devolver false si 1900 no es bisiesto" do
+    expect(esBisiesto(1900)).to eq false
+  end
+  it "deberia devolver false si 2100 no es bisiesto" do
+    expect(esBisiesto(2100)).to eq false
+  end
+  it "deberia devolver false 2200 false no es bisiesto" do
+    expect(esBisiesto(2200)).to eq false
+  end
+  it "deberia devolver false 2300 false no es bisiesto" do
+    expect(esBisiesto(2300)).to eq false
   end
 end
