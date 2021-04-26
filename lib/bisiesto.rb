916be6503a09f9ae81482628e8a2ae27964a8f81
Pablo Alvarez <79346499+p48102611@users.noreply.github.com>
2021-04-26 18:18:55 -0400

    Refactorizando condicionales a un solo if

--- lib/bisiesto.rb
+++ lib/bisiesto.rb
@@ -1,8 +1,5 @@
 def esBisiesto(num)
-    if num % 400 == 0
-        return true
-    end
-    if(num % 4 == 0 && num % 100 != 0)
+    if(num % 4 == 0 && num % 100 != 0 || num % 400 == 0)
         return true
     else
         return false
